8061cdca85bf843d5feeeb65e01d3ffae874d7ae
Michal M. <maraszm@gmail.com>
2016-02-09 12:50:15 +0100

    Update message alerts

--- src/main/java/pl/ekozefir/mobile/serial/centralstate/value/AlertMessageParser.java
+++ src/main/java/pl/ekozefir/mobile/serial/centralstate/value/AlertMessageParser.java
@@ -22,21 +22,20 @@ import pl.ekozefir.mobile.serial.centralstate.MobileParser;
 public class AlertMessageParser implements MobileParser {
 
     private static final Map<Integer, String> values = ImmutableMap.<Integer, String>builder().
-            put(0, "AWARIA_BRAK").
-            put(1, "AWARIA_CZUJNIKA").
-            put(2, "AWARIA_BAZY").
-            put(3, "AWARIA_TK_NAW").
-            put(4, "AWARIA_TK_WYW").
-            put(5, "AWARIA_BOT").
-            put(6, "AWARIA_ZNW").
-            put(7, "AWARIA_POLACZEN").
-            put(8, "AWARIA_PRZEGRZANIE").
-            put(9, "AWARIA_FILTR").
-            put(10, "AWARIA_FAKTURA_1").
-            put(11, "AWARIA_FAKTURA_2").
-            put(12, "AWARIA_ODSZRANIANIA").
-            put(13, "AWARIA_ROTOR").
-            put(53, "AWARIA_POLACZENIA").
+            put(0, "NO_FAILURE").
+            put(1, "TEMPERATURE_SENSOR_FAILURE").
+            put(2, "MAIN_BOARD_FAILURE").
+            put(3, "SUPPLY_FAN_FAILURE").
+            put(4, "EXTRACT_FAN_FAILURE").
+            put(5, "THERMAL_PROTECTION_ENABLED").
+            put(6, "WATER_HEATER_ANTIFROST_PROTECTION_ENABLED").
+            put(7, "CONNECTION_WITH_MAIN_BOARD_FAILURE").
+            put(8, "OVERHEATING").
+            put(9, "CHANGE_FILTER").
+            put(12, "DEFROSTING_FAILURE").
+            put(13, "ROTARY_EXCHANGER_FAILURE").
+            put(14, "FIRE_PROTECTION_ENABLED").
+            put(53, "EKOTOUCH_CONNECTION_FAILURE").
             build();
     private static final int byteNumber = 41;
 
